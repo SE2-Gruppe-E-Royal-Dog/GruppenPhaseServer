20b3ea755144a57eceafcfc518c7524b28691b09
Lukas Krall <lukaskr@edu.aau.at>
2022-04-11 14:15:46 +0200

    In Klasse Zahlenkarte einen Konstruktor hinzugefügt

--- src/main/java/com/uni/Kartendeck/Zahlenkarte.java
+++ src/main/java/com/uni/Kartendeck/Zahlenkarte.java
@@ -3,6 +3,10 @@ package com.uni.Kartendeck;
 public class Zahlenkarte extends Karte{
     private int wert;
 
+    public Zahlenkarte(int wert){
+        this.wert = wert;
+    }
+
     @Override
     public void ausspielen(){
 
