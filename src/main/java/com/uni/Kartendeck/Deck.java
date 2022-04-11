f1722e96416b26aa7966db5d9610bb05424631b4
Lukas Krall <lukaskr@edu.aau.at>
2022-04-11 13:43:55 +0200

    Klasse Deck erstellt

---
+++ src/main/java/com/uni/Kartendeck/Deck.java
@@ -0,0 +1,20 @@
+package com.uni.Kartendeck;
+
+import java.util.LinkedList;
+
+public class Deck {
+    private LinkedList<Karte> restliche_Karten;
+
+    public Deck(){
+        restliche_Karten = new LinkedList<>();
+        leeres_Deck_befüllen();
+    }
+
+    public void leeres_Deck_befüllen(){
+
+    }
+
+    private void deck_mischen(LinkedList<Karte> zuMischen){
+
+    }
+}
